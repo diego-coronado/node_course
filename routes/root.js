a9a369265b6fd264993cad25561df02e9f7e53e1
Diego Coronado <dcoronado@able.co>
2023-05-01 20:07:26 +0900

    add routes

---
+++ routes/root.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("^/$|/index(.html)?", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+router.get("/new-page(.html)?", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "new-page.html"));
+});
+
+router.get("/old-page(.html)?", (_, res) => {
+  res.redirect(301, "/new-page.html"); //302 by default
+});
+
+module.exports = router;
